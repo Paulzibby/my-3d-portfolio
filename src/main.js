acaf277a65ba04afe91d21f9f9d06cd1b37f4984
Paulzibby <112769710+Paulzibby@users.noreply.github.com>
2026-05-01 15:00:13 +0000

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -54,3 +54,4 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+console.log("Three.js is running!");
